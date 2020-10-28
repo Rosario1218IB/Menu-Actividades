4d036acf5f7579174fac0175dc7612fed0d1f08b
Maria Zuniga <charoth85@gmail.com>
2020-10-28 15:08:46 -0500

    Subiendo jquerie

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(main);
+var menuoculto=1;
+
+function main(){
+    $('.menu_bar').click(function(){
+        if(menuoculto==1){
+            $('nav').animate({
+                left:'0'
+            });
+            menuoculto = 0;
+        }
+        else{
+            $('nav').animate({
+                left:'-100%'
+            });
+            menuoculto=1;
+        }
+    });
+
+    $('.subMenu').click(function(){
+        $(this).children('.children').slideToogle();
+    });
+}
